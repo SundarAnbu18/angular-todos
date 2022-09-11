8aceb89953eb47fd0de3dd89da72af72e7e281fd
Sundar <sundaranbu007@gmail.com>
2022-09-11 17:22:28 +0530

    Angular Todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodosComponent } from './components/todos/todos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[];
+
+  inputTodo:string=""
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos=[
+
+    ]
+  }
+  toggleDone(id:number){
+    this.todos.map((v,i)=>{
+      if(i ==id ){
+        v.completed = !v.completed
+      }
+      return v
+    })
+
+  }
+  deleteTodo(id:number){
+    this.todos=this.todos.filter((v,i)=>
+      i !==id
+    )
+  }
+
+  AddTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed:false
+    })
+    this.inputTodo=""
+  }
+
+}
